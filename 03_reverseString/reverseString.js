498be782d3ede0fee67fd53e43a57df20d1a6165
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-06-04 16:25:35 +0200

    Finished exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let arraySplit = str.split("");
+  arraySplit = arraySplit.reverse();
+  return arraySplit.join("");
 };
 
 // Do not edit below this line
