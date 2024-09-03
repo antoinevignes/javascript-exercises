290f95f685bd9de8aefee9a41cb61393eb777a14
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-09-03 22:45:40 +0200

    Finished ex 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (numArg) {
+  // checks argument's type and makes sure we use
+  // a number throughout rest of function.
+  let num;
+  if (typeof numArg !== "number") {
+    num = parseInt(numArg);
+  } else {
+    num = numArg;
+  }
 
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for (let i = 2; i <= num; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+
+  return firstPrev;
 };
 
 // Do not edit below this line
